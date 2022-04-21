6ad053b8cc9891510b8281f23e59c217bf85dedd
xhcstudy <99958269+xhcstudy@users.noreply.github.com>
2022-04-21 20:32:56 +0800

    Add files via upload

---
+++ 01 C++ 书写Hello World/01 C++ 书写Hello World/01 C++ 书写Hello World.cpp
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+#define Day 7
+
+int main()
+{
+	int a = 10;
+	const int month = 12;
+
+	cout << "hello world" << endl;
+	cout << "a = " << a << endl;
+	cout << "一周总共有:" << Day << "天" << endl;
+	cout << "一年一共有:" << month << "个月份" << endl;
+	system("pause");
+
+	return 0;
+}
